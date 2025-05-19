29187f29de0b3780adad91d80711c6f5d1d23b2d
Benjamin Webb <benjamin.miller.webb@gmail.com>
2025-05-19 12:34:06 -0400

    Use primary reference server

--- src/lib/state/main/slice.ts
+++ src/lib/state/main/slice.ts
@@ -135,7 +135,7 @@ export const fetchDatasets = createAsyncThunk<
     string
 >('main/fetchDatasets', async (id: string) => {
     const response = await fetch(
-        `https://reference.geoconnex.dev/collections/mainstems/items/${id}`
+        `https://reference.geoconnex.us/collections/mainstems/items/${id}`
     );
     const data = (await response.json()) as Feature<
         Geometry,
