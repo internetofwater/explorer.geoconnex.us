c48ce7e5911d8e3a13dba3935abfdfc1544f9f0e
Emily DuBois <emilymdubois@gmail.com>
2026-09-23 08:32:28 -0500

    fix(ui): Use CQL2 filter syntax

--- src/app/features/SidePanel/Search.tsx
+++ src/app/features/SidePanel/Search.tsx
@@ -50,7 +50,7 @@ const Search: React.FC<Props> = (props) => {
                 controller.current = new AbortController();
 
                 const response = await fetch(
-                    `https://reference.geoconnex.us/collections/mainstems/items?sortby=-outlet_drainagearea_sqkm&filter=name_at_outlet+ILIKE+'%${query}%'+OR+uri+ILIKE+'%mainstems/${query}%'&f=json&skipGeometry=true`,
+                    `https://reference.geoconnex.us/collections/mainstems/items?sortby=-outlet_drainagearea_sqkm&filter-lang=cql2-text&filter=CASEI(name_at_outlet)+LIKE+CASEI('%${query}%')+OR+CASEI(uri)+LIKE+CASEI('mainstems/${query}%')&f=json&skipGeometry=true`,
                     { signal: controller.current.signal }
                 );
                 const data = (await response.json()) as FeatureCollection<
